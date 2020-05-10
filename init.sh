ce49c82793399419491020e2e1a34896b5a7a945
tc-imba <liuyh615@sjtu.edu.cn>
2020-05-10 18:25:01 +0800

    update readme and license, haven't tested on Linux

--- init.sh
+++ init.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+# init the submodule
 git submodule update --init --recursive
-cd elm-mdc
-make
+cd elm-mdc && make
 
+# download elm dependencies
 cd ..
 elm make
+
+# init python venv
+python3 -m virtualenv venv
+source venv/bin/activate
+pip3 install -e .
+
+# init node modules
+npm install -g yarn create-elm-app
+yarn
 
