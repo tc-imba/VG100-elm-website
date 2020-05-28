c1faea0d316716cdb26915b206639a3bb4f7581a
tc-imba <liuyh615@sjtu.edu.cn>
2020-05-29 02:09:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 git pull
 PUBLIC_URL='/vg100' node scripts/build.js
-FLASK_APP=backend FLASK_ENV=production flask run -h 0.0.0.0
+# FLASK_APP=backend FLASK_ENV=production flask run -h 0.0.0.0
+pm2 restart ./ecosystem.config.js
